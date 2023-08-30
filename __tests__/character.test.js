c1672ef7768a56343910eb3f25be4d6fe2308b4f
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-30 14:38:46 -0700

    Refactor tests, pass test 2
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -7,12 +7,14 @@ beforeEach(() => {
   newCharacter = new Character("Paul", 20, 20, 10);
 });
 
-
 test('it should return the properties of the Character class', () => {
-  expect(newCharacter).toEqual({name: "Paul", currentHp: 20, maxHp: 20, attackPoints: 10});
-})
+  expect(newCharacter).toEqual({name: "Paul", currentHp: 20, maxHp: 20, attackPoints: 10, armor: []});
+});
 
-// test('')
+test('it should add armor to newCharacter and increase maxHp by 7 points', () => {
+  newCharacter.addArmor("shield", 7);
+  expect(newCharacter).toEqual({name: "Paul", currentHp: 20, maxHp: 27, attackPoints: 10, armor: ["shield"]});
+});
 });
 
 
--- src/character.js
+++ src/character.js
@@ -4,5 +4,11 @@ export default class Character {
     this.currentHp = currentHp;
     this.maxHp = maxHp;
     this.attackPoints = attackPoints;
+    this.armor = [];
   }
-}
+  
+  addArmor(type, value){
+    this.armor.push(type);
+    this.maxHp += value;
+  }
+  }
