999ef94aa4597b4b42e5b59e61d4b5fbeaaae14e
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-30 13:31:31 -0700

    Good fail test 1
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,4 +1,16 @@
-// import Constructor from './../src/javascript.js';
+import Character from './../src/character.js';
+
+describe('Character', () => {
+
+test('it should return the properties of the Character class', () => {
+  const newCharacter = new Character("name1", "armor1", "weapon1");
+  expect(newCharacter).toEqual({name: "name1", armor: "armor1", weapon: "weapon1"});
+})
+
+});
+
+
+
 
 // describe('Constructor', () => {}
 // test() => {}
---
+++ src/character.js
@@ -0,0 +1,7 @@
+export default class Character {
+  constructor(name, armor, weapon){
+    this.name = name;
+    this.armor = armor;
+    this.weapon = weapon;
+  }
+}
